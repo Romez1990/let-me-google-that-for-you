7da2bc07bf1d03f6ebca5e752dd04c7720488a58
Romez <Romez1990@gmail.com>
2020-01-04 20:56:24 +0300

    feature(images): add images support

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 const withStylus = require('@zeit/next-stylus');
+const withImages = require('next-images');
 
 const dev = process.env.NODE_ENV !== 'production';
 
-module.exports = withStylus({
+module.exports = withStylus(withImages({
   cssModules: true,
   cssLoaderOptions: {
     importLoaders: 1,
     localIdentName: dev ? '[local]' : '[hash:base64:22]',
   }
-});
+}));
