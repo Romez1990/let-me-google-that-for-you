94e7d0e5f5701eabfa75b57a85e918ea0a6104ad
Romez <Romez1990@gmail.com>
2020-01-04 20:55:33 +0300

    feature(css): add css modules support with stylus

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
+const withStylus = require('@zeit/next-stylus');
+
 const dev = process.env.NODE_ENV !== 'production';
 
-module.exports = {};
+module.exports = withStylus({
+  cssModules: true,
+  cssLoaderOptions: {
+    importLoaders: 1,
+    localIdentName: dev ? '[local]' : '[hash:base64:22]',
+  }
+});
